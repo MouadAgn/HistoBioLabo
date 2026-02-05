ad0ba7f754a8eb900d14b52bb901e4009d17b350
MouadAgn <103049879+MouadAgn@users.noreply.github.com>
2026-02-05 14:46:18 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,16 +14,12 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
+        url: '/favicon.ico',
+        type: 'image/x-icon',
       },
       {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/favicon.png',
+        type: 'image/png',
       },
     ],
     apple: '/apple-icon.png',
